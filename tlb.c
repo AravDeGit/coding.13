57db526f2f6952093e080730de41cb70c4360f79
AravDeGit <dearav3@gmail.com>
2024-11-06 17:02:16 +0000

    Added timer for tlb

--- tlb.c
+++ tlb.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <time.h>
 
+void tlb(int n, int trials, int target) 
+{
+    time_t now;
+    time(&now);
+
+    struct tm *local = localtime(&now);
+
+    printf("The current time is: %s", asctime(local));
+
+    int *pages = (int*) malloc(n * sizeof(int));
+
+    for(int i = 0; i < n; i++)
+    {
+        
+    }
+}
